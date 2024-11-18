107e17180994416335ee68f152fffc35d141ae7c
r_wai <r_wai@DESKTOP-S0U00BI>
2024-11-18 10:28:10 +0200

    Add project files.

---
+++ WebAppGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
